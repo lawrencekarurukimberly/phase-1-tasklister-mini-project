677917113c4dd76df12113d3997c82e403336b29
lawrencek <spiceeast509@gmail.com>
2024-10-15 11:26:06 +0300

    origin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',()=>{
+     let form = document.querySelector('#create-task-form')
+    form.addEventListener('submit', (e)=> {
+      e.preventDefault()
+      let todo = document.getElementById ('new-task-description').value
+      buildToDo(todo)
+      form.reset()
+    })
+
+  })
+  function buildToDo(todo){
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.context = 'x'
+    li.textContent = `${todo}`
+    li.appendChild(btn)
+    console.log(li)
+    document.querySelector('#tasks').appendChild(li)
+  }
+  function handleDelete(e){
+    e.target.paentNode.remove()
+  }
